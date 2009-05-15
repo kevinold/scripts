037f9b7ab51452db295c6a655eadbd3d47bdd892
Kevin Old <kevin@kevinold.com>
2009-05-14 19:50:09 -0500

    install git script

---
+++ install_git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Download and install Git
+
+version=1.6.3.1
+cd /tmp
+wget http://www.kernel.org/pub/software/scm/git/git-$version.tar.bz2
+wget http://www.kernel.org/pub/software/scm/git/git-manpages-$version.tar.bz2
+
+#install git
+tar -xf git-$version.tar.bz2
+cd git-$version
+./configure --prefix=/usr/local
+make all
+sudo make install
+
+# install manpages
+cd /tmp
+sudo tar -xv -C /usr/local/share/man -f git-manpages-$version.tar.bz2
